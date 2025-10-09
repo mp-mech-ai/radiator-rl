b779574e22eddd53d4e1bb74da5f4329fe64c592
Malo POTEREAU <malo.potereau@gmail.com>
2025-10-09 12:15:55 +0200

    parallelization and tests folder

--- radiator_rl/agents/dqn_agent.py
+++ radiator_rl/agents/dqn_agent.py
@@ -7,10 +7,8 @@ from radiator_rl.envs.house_env import HouseEnv
 from collections import deque
 import matplotlib.pyplot as plt
 import pandas as pd
-import os
 from gymnasium.vector import AsyncVectorEnv, SyncVectorEnv
 from datetime import datetime
-import time
 
 class DQNAgent:
     def __init__(
@@ -200,7 +198,8 @@ class DQNAgent:
                         # Mark as done if environment terminated
                         if dones_step[i]:
                             dones[i] = True
-                
+
+
                 # Optimize model if training
                 if is_training and len(memory) >= self.batch_size:
                     self.optimize_model(memory, optimizer)
@@ -209,7 +208,6 @@ class DQNAgent:
                     # Update target network periodically
                     if is_training and self.optimization_steps % self.target_update_freq == 0:
                         self.target_dqn.load_state_dict(self.policy_dqn.state_dict())
-                        # print(f"Target network updated at optimization step {self.optimization_steps} (Episode {episode})")
                 
                 # Decay epsilon
                 if is_training:
@@ -226,6 +224,7 @@ class DQNAgent:
             plt.ioff()
             plt.show()
         envs.close()
+
         return reward_per_episode
 
     def optimize_model(self, memory, optimizer):
@@ -286,35 +285,4 @@ class DQNAgent:
             T_out_measurement.append(list(df.iloc[ind:ind+step_per_day, 1]))
         
         return T_out_measurement, dt, start_time
-
-
-if __name__ == "__main__":
-    num_workers = len(os.sched_getaffinity(0))
-    print(f"num_worker: {num_workers}")
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    t0 = time.time()
-    agent = DQNAgent(
-        hidden_dim=128, 
-        num_layers=2, 
-        output_dim=5,
-        device=device,
-        data_path="data/clean/t_out.csv",
-        num_workers=1
-        )
-    rewards = agent.run(is_training=True, render=False, episodes=10)
-    print(agent.optimization_steps)
-    print(f"---Not parallelized: {time.time() - t0:.2f}---")
-
-    t0 = time.time()
-    agent = DQNAgent(
-        hidden_dim=128, 
-        num_layers=2, 
-        output_dim=5,
-        device=device,
-        data_path="data/clean/t_out.csv",
-        num_workers=num_workers
-        )
-    rewards = agent.run(is_training=True, render=False, episodes=10)
-    print(agent.optimization_steps)
-    print(f"---Parallelized: {time.time() - t0:.2f}---")
 
---
+++ tests/parallelization.py
@@ -0,0 +1,39 @@
+import os
+import time
+from radiator_rl.agents.dqn_agent import DQNAgent
+import torch
+"""
+Parallelization is only effective if the episode duration is long or if the calculation of each
+steps are costly. If on of those conditions aren't met you should consider training sequentially.
+
+In all case, it is recommended to time a single episode with 1 worker and N worker and see if 
+it is efficient.
+"""
+num_workers = len(os.sched_getaffinity(0))
+print(f"num_worker: {num_workers}")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+t0 = time.time()
+agent = DQNAgent(
+    hidden_dim=128, 
+    num_layers=2, 
+    output_dim=5,
+    device=device,
+    data_path="data/clean/t_out.csv",
+    num_workers=1
+    )
+rewards = agent.run(is_training=True, render=False, episodes=5)
+print(agent.optimization_steps)
+print(f"---Not parallelized: {time.time() - t0:.2f}s ---")
+
+t0 = time.time()
+agent = DQNAgent(
+    hidden_dim=128, 
+    num_layers=2, 
+    output_dim=5,
+    device=device,
+    data_path="data/clean/t_out.csv",
+    num_workers=num_workers
+    )
+rewards = agent.run(is_training=True, render=False, episodes=5)
+print(agent.optimization_steps)
+print(f"---Parallelized: {time.time() - t0:.2f}s ---")
